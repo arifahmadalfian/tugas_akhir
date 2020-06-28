8f895da0cbaec747f760a91128f54cfa40c2c0f5
arifahmadalfian <arifahmadalfian@gmail.com>
2020-06-28 17:50:50 +0700

    Mengedit tampilan di activity Detail beserta rsc layout-nya

--- app/src/main/java/com/arifahmadalfian/rdsmapbox/adapter/SearchAdapter.kt
+++ app/src/main/java/com/arifahmadalfian/rdsmapbox/adapter/SearchAdapter.kt
@@ -14,14 +14,13 @@ class SearchAdapter(var context: Context, var pelanggan: List<Pelanggan>): Recyc
 
     inner class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        var id: TextView = itemView.findViewById(R.id.tv_item_id)
         var nama: TextView = itemView.findViewById(R.id.tv_item_nama)
         var alamatPemesan: TextView = itemView.findViewById(R.id.tv_item_alamat_pemesan)
         var alamatDikirim: TextView = itemView.findViewById(R.id.tv_item_alamat_dikirim)
         var latitude: TextView = itemView.findViewById(R.id.tv_item_latitude)
         var longitude: TextView = itemView.findViewById(R.id.tv_item_longitude)
         var keterangan: TextView = itemView.findViewById(R.id.tv_item_keterangan)
-        var photo: TextView = itemView.findViewById(R.id.tv_item_photo)
+       // var photo: TextView = itemView.findViewById(R.id.tv_item_photo)
         var telepon: TextView = itemView.findViewById(R.id.tv_item_telepon)
     }
 
@@ -36,14 +35,13 @@ class SearchAdapter(var context: Context, var pelanggan: List<Pelanggan>): Recyc
     }
 
     override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
-        holder.id.text = pelanggan[position].id.toString()
         holder.nama.text = pelanggan[position].nama
         holder.alamatPemesan.text = pelanggan[position].alamat_pemesan
         holder.alamatDikirim.text = pelanggan[position].alamat_dikirim
         holder.longitude.text = pelanggan[position].longitude.toString()
         holder.latitude.text = pelanggan[position].latitude.toString()
         holder.keterangan.text = pelanggan[position].keterangan
-        holder.photo.text = pelanggan[position].photo.toString()
+        //holder.photo.text = pelanggan[position].photo.toString()
         holder.telepon.text = pelanggan[position].telepon
     }
 }
