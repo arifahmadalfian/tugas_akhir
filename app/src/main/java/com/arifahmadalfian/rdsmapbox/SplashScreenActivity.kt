8c5b4ea59131eb0736732f617368812c83179e93
arifahmadalfian <arifahmadalfian@gmail.com>
2020-07-25 11:25:21 +0700

    menambahkan SplashScreenActivity.kt new

--- app/src/main/java/com/arifahmadalfian/rdsmapbox/SplashScreenActivity.kt
+++ app/src/main/java/com/arifahmadalfian/rdsmapbox/SplashScreenActivity.kt
@@ -5,22 +5,35 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity: AppCompatActivity() {
 
-    lateinit var handler: Handler
+    //lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash_screen)
 
+        splash_screen.alpha = 0f
+
+        splash_screen.animate().setDuration(1700).alpha(1f).withEndAction{
+            val intent = Intent(this@SplashScreenActivity, LoginActivity:: class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+
+        /*
         handler = Handler()
         handler.postDelayed({
             val intent = Intent(this@SplashScreenActivity, LoginActivity:: class.java)
             startActivity(intent)
             finish()
         }, 3000)  //delay 3 detik
+
+         */
 
     }
 }
