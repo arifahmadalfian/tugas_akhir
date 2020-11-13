49d8dc1b225579dd2076eee065bf4409aa72d09f
arifahmadalfian <arifahmadalfian@gmail.com>
2020-11-13 21:11:54 +0700

    version 1.0 Migrasi Database menggunakan Firebase Membuat activity register

--- app/src/main/java/com/arifahmadalfian/rdsmapbox/LoginActivity.kt
+++ app/src/main/java/com/arifahmadalfian/rdsmapbox/LoginActivity.kt
@@ -3,23 +3,14 @@ package com.arifahmadalfian.rdsmapbox
 import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import com.arifahmadalfian.rdsmapbox.database.Database
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_about.view.*
 import kotlinx.android.synthetic.main.activity_login.*
-import java.util.*
 
 class LoginActivity : AppCompatActivity(){
 
     private val firebase = FirebaseAuth.getInstance()
-    @Suppress("DEPRECATION")
-    var progresDialog: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,25 +25,14 @@ class LoginActivity : AppCompatActivity(){
                 password.isEmpty() -> et_login_password.error = "Password tidak boleh kosong"
                 else -> loginToFirebase()
             }
-            //loading di jalankan
-            //toProgresDialog()
 
-            //loading di hentikan
-           // progresDialog?.dismiss()
+        }
+
+        btn_login_to_register.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
-/*
-    private fun toProgresDialog() {
-        @Suppress("DEPRECATION")
-        progresDialog = ProgressDialog(this)
-        progresDialog?.show()
-        progresDialog?.setContentView(R.layout.progres_dialog)
-        progresDialog?.window?.setBackgroundDrawableResource(
-            android.R.color.transparent
-        )
-    }
-
- */
 
     private fun loginToFirebase() {
 
@@ -71,6 +51,5 @@ class LoginActivity : AppCompatActivity(){
                 Toast.makeText(this@LoginActivity,"Tidak terhubung internet",Toast.LENGTH_SHORT).show()
             }
     }
-
 
 }
