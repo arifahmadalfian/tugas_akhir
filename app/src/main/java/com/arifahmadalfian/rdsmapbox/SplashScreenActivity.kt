b30a1d87f81980c727e27e19f19ea05d89320182
arifahmadalfian <arifahmadalfian@gmail.com>
2020-11-21 13:13:14 +0700

    Menghapus Halaman Utama

--- app/src/main/java/com/arifahmadalfian/rdsmapbox/SplashScreenActivity.kt
+++ app/src/main/java/com/arifahmadalfian/rdsmapbox/SplashScreenActivity.kt
@@ -3,10 +3,6 @@ package com.arifahmadalfian.rdsmapbox
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import com.arifahmadalfian.rdsmapbox.activity.Home
-import com.arifahmadalfian.rdsmapbox.activity.ui.home.HomeFragment
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 
@@ -19,7 +15,7 @@ class SplashScreenActivity: AppCompatActivity() {
         splash_screen.alpha = 0f
 
         splash_screen.animate().setDuration(1700).alpha(1f).withEndAction{
-            val intent = Intent(this@SplashScreenActivity, Home:: class.java)
+            val intent = Intent(this@SplashScreenActivity, HomeActivity:: class.java)
             startActivity(intent)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
