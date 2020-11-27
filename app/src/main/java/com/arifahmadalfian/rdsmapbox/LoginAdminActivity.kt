f3c27ad6f557342032182370ace2557222a05057
arifahmadalfian <arifahmadalfian@gmail.com>
2020-11-27 19:11:27 +0700

    Menambahkan Halaman utama. memilih login admin atau login pengirim

---
+++ app/src/main/java/com/arifahmadalfian/rdsmapbox/LoginAdminActivity.kt
@@ -0,0 +1,11 @@
+package com.arifahmadalfian.rdsmapbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginAdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_admin)
+    }
+}
---
+++ app/src/main/java/com/arifahmadalfian/rdsmapbox/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.arifahmadalfian.rdsmapbox
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_main_pengirim.setOnClickListener {
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+        btn_main_admin.setOnClickListener {
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
